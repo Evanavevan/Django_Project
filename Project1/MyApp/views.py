a7148c732cc4ec8a0c327a07e668e485c429f14c
Evanavevan <1365205439@qq.com>
2018-12-28 12:19:32 +0800

    第二次修改

--- Project1/MyApp/views.py
+++ Project1/MyApp/views.py
@@ -2,34 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Grades, Students
 
-#定义视图
+
+# 定义视图
 def index(request):
     return HttpResponse('evan is a handsome and great man!')
+
 
 def detail(request, num):
     return HttpResponse('detail-{}'.format(num))
 
 
-from .models import Grades, Students
 def grades(request):
-    #去模板里取数据
+    # 去模板里取数据
     gradeList = Grades.objects.all()
-    #将数据传递给模板，模板渲染页面，其后返回给浏览器
-    return render(request, 'MyApp/grades.html', {'grades':gradeList})
+    # 将数据传递给模板，模板渲染页面，其后返回给浏览器
+    return render(request, 'MyApp/grades.html', {'grades': gradeList})
+
 
 def students(request):
-    #去模板里取数据
+    # 去模板取数据
     studentsList = Students.objects.all()
-    #将数据传递给模板，模板渲染页面，其后返回给浏览器
-    return render(request, 'MyApp/students.html', {'students':studentsList})
+    # 将数据传递给模板，模板渲染页面，其后返回给浏览器
+    return render(request, 'MyApp/students.html', {'students': studentsList})
+
 
 def gradeStudents(request, num):
     grade = Grades.objects.get(pk=num)
     studentsList = grade.students_set.all()
-    return render(request, 'MyApp/students.html', {'students':studentsList})
-
-
+    return render(request, 'MyApp/students.html', {'students': studentsList})
 
 
 def addstudent(request):
